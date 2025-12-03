8d476db25023b89539f99ed27a4d032cdd3202e8
freddyghabel-ops <freddyghabel@gmail.com>
2025-12-03 05:22:37 +0300

    Update server.js

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.post("/pay", async (req, res) => {
       external_reference: reference,
       customer_name: "Customer",
       callback_url: "https://swiftloanback.onrender.com/callback",
-      channel_id: "000235"
+      channel_id: "000259"
     };
 
     const url = "https://swiftwallet.co.ke/pay-app-v2/payments.php";
